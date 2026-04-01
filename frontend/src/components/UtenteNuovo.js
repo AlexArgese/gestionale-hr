cb9fd6a0e1afd3575f1f0d9db099ff72eb143fdb
AlexArgese <101403816+AlexArgese@users.noreply.github.com>
2026-04-01 23:40:37 +0200

    excel update

--- frontend/src/components/UtenteNuovo.js
+++ frontend/src/components/UtenteNuovo.js
@@ -321,8 +321,13 @@ function UtenteNuovo({
                 value={form.tipo_contratto}
                 onChange={onChange("tipo_contratto")}
               >
-                <option value="full_time">Full time</option>
-                <option value="part_time">Part time</option>
+                <option value="full_time">Full time 6,40</option>
+                <option value="part_time_2">Part time 2</option>
+                <option value="part_time_3">Part time 3</option>
+                <option value="part_time_4">Part time 4</option>
+                <option value="part_time_6">Part time 6</option>
+                <option value="part_time_8">Part time 8</option>
+                <option value="chiamata_6">Chiamata 6</option>
               </select>
             </div>
           </div>
